fe6b4315f94732a7a96f287ef169d4fd895b6d86
just3is-dev <a.yalaev@icloud.com>
2023-05-27 23:16:54 +0300

    basics/VueCalculator: добавлен калькулятор

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,48 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+createApp({
+  name: 'VueCalculator',
+  data() {
+    return {
+      firstNumber: 0,
+      secondNumber: 0,
+      operator: 'sum',
+    }
+  },
+  computed: {
+    result() {
+      switch (this.operator) {
+        case 'subtract':
+          return this.firstNumber - this.secondNumber;
+        case 'multiply':
+          return this.firstNumber * this.secondNumber;
+        case 'divide':
+          return this.firstNumber / this.secondNumber;
+        default:
+          return this.firstNumber + this.secondNumber;
+      }
+    }
+  },
+  template: `
+    <div class="row">
+      <div class="col">
+        <input type="number" v-model="firstNumber" />
+      </div>
+
+      <div class="col" style="display: flex; flex-direction: column; font-family: emoji">
+        <label><input type="radio" name="operator" v-model="operator" value="sum" /> ➕</label>
+        <label><input type="radio" name="operator" v-model="operator" value="subtract" /> ➖</label>
+        <label><input type="radio" name="operator" v-model="operator" value="multiply" /> ✖</label>
+        <label><input type="radio" name="operator" v-model="operator" value="divide" /> ➗</label>
+      </div>
+
+      <div class="col">
+        <input type="number" v-model="secondNumber" />
+      </div>
+
+      <div class="col">=</div>
+
+      <output class="col" v-model="result">{{ result }}</output>
+    </div>
+  `
+}).mount('#app');
