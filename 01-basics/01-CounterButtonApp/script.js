fa0a850ad001c487a9a19019b921c7fb6e096a3b
just3is-dev <a.yalaev@icloud.com>
2023-05-27 22:17:55 +0300

    basics/CounterButtonApp: добавлена кнопка со счетчиком

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,11 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import {createApp, defineComponent} from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const ButtonComponent = defineComponent({
+  name: 'ButtonComponent',
+  data: () => ({
+    counter: 0,
+  }),
+  template: `<button type="button" @click="counter+=1">{{counter}}</button>`
+});
+
+createApp(ButtonComponent).mount('#app');
