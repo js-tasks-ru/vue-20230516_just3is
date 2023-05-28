018c88cdfdc908c79f0ccaccc8715fd98cfcd1b6
just3is-dev <a.yalaev@icloud.com>
2023-05-28 12:59:52 +0300

    basics/MeetupTitle: добавлена загрузка информации о митапе по ид

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,30 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+createApp({
+  name: 'MeetupTitle',
+  data() {
+    return {
+      title: '',
+      meetupId: null,
+    }
+  },
+  watch: {
+    meetupId(id) {
+      fetchMeetupById(id).then((data) => this.title = data.title)
+    }
+  },
+  template: `
+    <div>
+      <label><input type="radio" name="meetupId" value="1" v-model="meetupId" /> 1</label>
+      <label><input type="radio" name="meetupId" value="2" v-model="meetupId" /> 2</label>
+      <label><input type="radio" name="meetupId" value="3" v-model="meetupId" /> 3</label>
+      <label><input type="radio" name="meetupId" value="4" v-model="meetupId" /> 4</label>
+      <label><input type="radio" name="meetupId" value="5" v-model="meetupId" /> 5</label>
+
+      <hr />
+
+      <h3>{{ title }}</h3>
+    </div>
+  `
+}).mount('#app')
