31290f92ca9f605f367456d59d1355bc3427cb10
just3is-dev <a.yalaev@icloud.com>
2023-06-03 23:41:00 +0300

    feature/components: добавлен компонент MeetupDescription

--- 02-components/02-MeetupDescription/components/MeetupDescription.js
+++ 02-components/02-MeetupDescription/components/MeetupDescription.js
@@ -3,5 +3,7 @@ import { defineComponent } from '../vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupDescription',
 
-  template: `<p class="meetup-description">Описание должно быть здесь</p>`,
+  props: ['description'],
+
+  template: `<p class="meetup-description">{{ description }}</p>`,
 });
